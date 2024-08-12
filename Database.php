afafe4b53466dd7cee82d9eae58aae577d0abb5c
Theo Wright <theowright2015@gmail.com>
2024-08-12 16:36:24 +0100

    2 - added mysql database and connection

---
+++ Database.php
@@ -0,0 +1,25 @@
+<?php
+
+class Database {
+    public $connection;
+    public function __construct($config, $username = 'root', $password = 'newpassword') {
+     
+
+        $configString = "mysql:" . http_build_query($config, '', ';');
+
+        $dsn = $configString;
+
+        $this->connection = new PDO($dsn, $username, $password, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+    }
+    public function queryAll($query, $params = []) {
+    
+        
+        $statement = $this->connection->prepare($query);
+        
+        $statement->execute($params);
+        
+        return $statement->fetchAll();
+    }
+};
--- index.php
+++ index.php
@@ -1,4 +1,30 @@
 <?php
 
 require "functions.php";
-require "router.php";
+// require "router.php";
+require "Database.php";
+
+// connect to db and execute query
+$config = require "config.php";
+
+
+
+$db = new Database($config['database']);
+
+$id = $_GET['id'];
+$query = "select * from posts where id = :id";
+
+
+/**
+ * bound parameters to prevent SQL injection
+ *   - provide query and param separately
+ */
+$posts = $db->queryAll($query, [':id' => $id]);
+
+
+
+foreach ($posts as $post) {
+    echo "<li>{$post['title']}</li>";
+}
+
+// dumpAndDie($posts);
