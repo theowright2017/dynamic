5c55b3148c94220b58a8982d35ef1f21cd2b5881
Theo Wright <theowright2015@gmail.com>
2024-08-15 14:22:02 +0100

    notes 7 - auth - up to inc 40

--- views/index.view.php
+++ views/index.view.php
@@ -24,7 +24,7 @@
         <?php require base_path("views/partials/banner.php"); ?>
         <main>
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <p>HELLOOOO</p>
+            <p>Hello, <?= $_SESSION['user']['email'] ?? 'Guest' ?>. Welcome to the home page.</p>
             </div>
         </main>
     </div>
